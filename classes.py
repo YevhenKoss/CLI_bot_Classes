64b03521c7c0e08b36d9cadec012a22ff2755d05
v.dunkan <v.dunkan@gmail.com>
2023-01-10 22:40:22 +0200

    by Skiff

--- classes.py
+++ classes.py
@@ -1,16 +1,125 @@
 from collections import UserDict
+from datetime import datetime
+
 from validation import *
 
 
-class AddressBook(UserDict):
+class Field:
+    def __init__(self, value) -> None:
+        self.__value = None
+        self.value = value
 
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        self.__value = value
+
+    def __repr__(self) -> str:
+        return f"{self.value}"
+
+    def __str__(self) -> str:
+        return f"{self.value}"
+
+    def __eq__(self, __o: object) -> bool:
+        if self.value == __o.value:
+            return True
+        return False
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        self.__value = phone_number_validation(value)
+
+
+class Birthday(Field):
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        self.__value = birthday_validation(value)
+
+
+class Record:
+    def __init__(self, name: Name, phone: Phone = None, birthday: Birthday = None):
+        self.name = name
+        self.phones = []
+        if phone:
+            self.phones.append(phone)
+        self.birthday = birthday
+
+    def __repr__(self):
+        # if self.phones and self.birthday:
+        return f"{self.name.value} {','.join([p.value for p in self.phones]) if self.phones else ''} {self.birthday if self.birthday else ''}"
+        # elif self.phones and not self.birthday:
+        #     return f"{self.name.name}, {self.phones}"
+        # elif self.birthday and not self.phones:
+        #     return f"{self.name.name}, {self.birthday}"
+        # else:
+        #     return f"{self.name.name}"
+
+    def add(self, phone: Phone):
+        if isinstance(phone, Phone):
+            self.phones.append(phone)
+        return f"phone {phone.value} add successful to contact {self.name.value}"
+
+    def edit(self, old_phone, new_phone):
+        if old_phone in self.phones:
+            self.phones.remove(old_phone)
+            self.phones.append(new_phone)
+        else:
+            print(f"In this record no phone {old_phone}")
+
+    def remove(self, old_phone):
+        if old_phone in self.phones:
+            self.phones.remove(old_phone)
+        else:
+            print(f"In this record no phone {old_phone}")
+
+    def days_to_birthday(self):
+        # print(f"This is {self.name}"+"'s "+"contact.")
+        if self.birthday:
+            # self.birthday = self.birthday.birthday ???????
+            today = datetime.today()
+            # dob_data = self.birthday.split(".")
+            # dob_day = int(dob_data[0])
+            # dob_month = int(dob_data[1])
+            # this_year = today.year
+            next_birthday = self.birthday.value.replace(year=today.year)
+            if today < next_birthday:
+                gap = (next_birthday - today).days
+                return "This contacts birthday is in " + str(gap) + " days."
+            elif today == next_birthday:
+                return "Today is contacts's birthday! Happy Birthday!"
+            else:
+                next_birthday = self.birthday.value.replace(year=today.year + 1)
+                gap = (next_birthday - today).days
+                return "This contacts birthday is in " + str(gap) + " days."
+        else:
+            return "Current contact doesn't have date of birth"
+
+
+class AddressBook(UserDict):
     def __init__(self):
         super().__init__()
         self.index = None
 
-    def add_record(self, record):
+    def add_record(self, record: Record):
         self.index = len(self.data) + 1
-        self.data[self.index] = record
+        self.data[record.name.value] = record
 
     def show(self, start=1, end=1):
         if start <= end <= len(self.data):
@@ -52,106 +161,24 @@ class AddressBook(UserDict):
             return f"{record.name}"
 
 
-class Record:
+if __name__ == "__main__":
+    ab = AddressBook()
+    name1 = Name("Bill")
+    phone1 = Phone("+380996787878")
+    rec1 = Record(name1, phone1)
 
-    def __init__(self, name, phone=None, birthday=None):
-        self.name = name
-        self.phones = []
-        if phone:
-            self.phones.append(phone)
-        self.birthday = birthday
+    rec2 = Record(Name("Jill"), Phone("+389671234545"), Birthday("12.03.1995"))
 
-    def __repr__(self):
-        if self.phones and self.birthday:
-            return f"{self.name.name}, {self.phones}, {self.birthday}"
-        elif self.phones and not self.birthday:
-            return f"{self.name.name}, {self.phones}"
-        elif self.birthday and not self.phones:
-            return f"{self.name.name}, {self.birthday}"
-        else:
-            return f"{self.name.name}"
+    ab.add_record(rec1)
+    ab.add_record(rec2)
 
-    def add(self, new_phone):
-        self.phones.append(new_phone)
+    phone3 = Phone("0453423434")  # This line must rise exceptions!
 
-    def edit(self, old_phone, new_phone):
-        if old_phone in self.phones:
-            self.phones.remove(old_phone)
-            self.phones.append(new_phone)
-        else:
-            print(f"In this record no phone {old_phone}")
+    rec1.add(phone3)
 
-    def remove(self, old_phone):
-        if old_phone in self.phones:
-            self.phones.remove(old_phone)
-        else:
-            print(f"In this record no phone {old_phone}")
+    phone4 = Phone("+380996787878")
+    phone5 = Phone("+380667877676")
 
-    def days_to_birthday(self):
-        print(f"This is {self.name}"+"'s "+"contact.")
-        if self.birthday is not None:
-            self.birthday = self.birthday.birthday
-            today = date.today()
-            dob_data = self.birthday.split(".")
-            dob_day = int(dob_data[0])
-            dob_month = int(dob_data[1])
-            this_year = today.year
-            next_birthday = date(this_year, dob_month, dob_day)
-            if today < next_birthday:
-                gap = (next_birthday - today).days
-                print("This abonent birhday is in " + str(gap) + " days.")
-            elif today == next_birthday:
-                print("Today is abonent's birthday! Happy Birthday!")
-            else:
-                next_birthday = date(this_year + 1, dob_month, dob_day)
-                gap = (next_birthday - today).days
-                print("This abonent birthday is in " + str(gap) + " days.")
-        else:
-            print("Current contact doesn't have date of birth")
+    rec1.edit(phone4, phone5)
 
-
-class Name:
-
-    def __init__(self, name):
-        self.name = name
-
-    def __repr__(self):
-        return f"{self.name}"
-
-
-class Fild:
-    pass
-
-
-class Phone(Fild):
-
-    def __init__(self, value):
-        self.__value = phone_number_validation(value)
-
-    def __repr__(self):
-        return f"{self.__value}"
-
-    @property
-    def value(self):
-        return self.__value
-
-    @value.setter
-    def value(self, new_value):
-        self.__value = phone_number_validation(new_value)
-
-
-class Birthday(Fild):
-
-    def __init__(self, birthday):
-        self.__birthday = birthday_validation(birthday)
-
-    def __repr__(self):
-        return f"{self.__birthday}"
-
-    @property
-    def birthday(self):
-        return self.__birthday
-
-    @birthday.setter
-    def birthday(self, birthday):
-        self.__birthday = birthday_validation(birthday)
+    print(ab)
--- validation.py
+++ validation.py
@@ -1,4 +1,5 @@
-from datetime import date
+from datetime import datetime
+
 
 def phone_number_validation(phone):
     if isinstance(phone, str):
@@ -11,32 +12,37 @@ def phone_number_validation(phone):
         .replace("(", "")
         .replace(")", "")
         .replace("-", "")
-        .replace(" ", ""))
+        .replace(" ", "")
+    )
     if phone.isdigit() and len(phone) == 12:
         phone = phone
     else:
-        print(f"Phone {phone} is not saved!\nWrong phone number format! Try '+XX(XXX)XXX-XX-XX'")
-        phone = None
+        raise ValueError(
+            f"Phone {phone} is not saved!\nWrong phone number format! Try '+XX(XXX)XXX-XX-XX'"
+        )
     return phone
 
 
 def birthday_validation(birthday):
-    birthday = str(birthday)
-    birthday = (
-        birthday.strip()
-        .replace("/", ".")
-        .replace("-", ".")
-        .replace(" ", "."))
-    dob_data = birthday.split(".")
-    today = date.today().year
-    if dob_data[0].isdigit() and 0 < int(dob_data[0]) <= 31:
-        if dob_data[1].isdigit() and 0 < int(dob_data[1]) <= 12:
-            if dob_data[2].isdigit() and 0 < int(dob_data[2]) <= today and len(dob_data[2]) == 4:
-                birthday = birthday
-                return birthday
-            else:
-                print(f"Date of birth {birthday} is not saved!\nYou have to enter numbers separated by a dot: DD.MM.YYYY")
-        else:
-            print(f"Date of birth {birthday} is not saved!\nYou have to enter numbers separated by a dot: DD.MM.YYYY")
-    else:
-        print(f"Date of birth {birthday} is not saved!\nYou have to enter numbers separated by a dot: DD.MM.YYYY")
+    # birthday = str(birthday)
+    # birthday = (
+    #     birthday.strip()
+    #     .replace("/", ".")
+    #     .replace("-", ".")
+    #     .replace(" ", "."))
+    # dob_data = birthday.split(".")
+    # today = date.today().year
+    # if dob_data[0].isdigit() and 0 < int(dob_data[0]) <= 31:
+    #     if dob_data[1].isdigit() and 0 < int(dob_data[1]) <= 12:
+    #         if dob_data[2].isdigit() and 0 < int(dob_data[2]) <= today and len(dob_data[2]) == 4:
+    #             birthday = birthday
+    #             return birthday
+    try:
+        return datetime.strptime(str(birthday), "%d.%m.%Y").date()
+    except ValueError:
+        return f"Date of birth {birthday} is not saved!\nYou have to enter numbers separated by a dot: DD.MM.YYYY"
+
+
+if __name__ == "__main__":
+    print(birthday_validation("10.12.1985"))
+    print(birthday_validation("10-12-1985"))
